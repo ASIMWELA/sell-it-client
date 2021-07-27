e58e03e79648e5998e01bf9c4b54a3560ec2da9b
ASIMWELA <augastinesimwela@gamil.com>
2021-07-27 22:41:17 +0200

    customer home design

--- app/src/main/java/com/main/sellit/ui/customer/CustomerHomeActivity.java
+++ app/src/main/java/com/main/sellit/ui/customer/CustomerHomeActivity.java
@@ -1,8 +1,10 @@
 package com.main.sellit.ui.customer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 import com.main.sellit.R;
 
@@ -13,9 +15,26 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CustomerHomeActivity extends AppCompatActivity {
 
+    RelativeLayout btnLogout;
+    CardView cvOpenAppointmentsActivity, cvOpenServicesActivity, cvOpenProfileActivity, cvOpenRequestsActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_home);
+        initViews();
+
+
     }
+
+
+
+    private void initViews() {
+        cvOpenAppointmentsActivity = findViewById(R.id.cv_customer_home_open_appointments_activity);
+        cvOpenProfileActivity = findViewById(R.id.cv_customer_home_open_profile_activity);
+        cvOpenRequestsActivity = findViewById(R.id.cv_customer_home_open_service_request_activity);
+        cvOpenServicesActivity = findViewById(R.id.cv_customer_home_open_services_activity);
+        btnLogout = findViewById(R.id.rl_customer_home_logout_btn);
+    }
+
 }
