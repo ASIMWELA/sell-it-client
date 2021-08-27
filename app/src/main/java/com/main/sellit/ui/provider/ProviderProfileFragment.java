c081119eead20cb669986f231f4d5bd3de3b15fb
ASIMWELA <augastinesimwela@gamil.com>
2021-08-27 22:39:31 +0200

    procider home design

--- app/src/main/java/com/main/sellit/ui/provider/ProviderProfileFragment.java
+++ app/src/main/java/com/main/sellit/ui/provider/ProviderProfileFragment.java
@@ -7,65 +7,66 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.main.sellit.R;
+import com.main.sellit.helper.SessionManager;
+import com.main.sellit.model.CustomerLoginModel;
+import com.main.sellit.model.ProviderLoginModel;
 
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProviderProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ProviderProfileFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProviderProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProviderProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProviderProfileFragment newInstance(String param1, String param2) {
-        ProviderProfileFragment fragment = new ProviderProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    TextView tvProviderUsername, tvProviderFullName,
+    tvProviderEmail, tvProviderPhoneNumber, tvProviderOfficeLocation,
+    tvProviderBusinessDesc;
+    SessionManager sessionManager;
+    ProviderLoginModel  providerLoginModel;
+    ImageView ivLogout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_provider_profile, container, false);
+        View view =  inflater.inflate(R.layout.fragment_provider_profile, container, false);
+        initView(view);
+        sessionManager = new SessionManager(requireContext());
+        Gson gson = new Gson();
+        String json = sessionManager.getLoggedInUser();
+        providerLoginModel = gson.fromJson(json, ProviderLoginModel.class);
+        String fullName = providerLoginModel.getFirstName() + "  "+providerLoginModel.getLastName();
+
+        //set to views
+        tvProviderBusinessDesc.setText(providerLoginModel.getProviderDescription());
+        tvProviderFullName.setText(fullName);
+        tvProviderUsername.setText(providerLoginModel.getUserName());
+        tvProviderOfficeLocation.setText(providerLoginModel.getOfficeAddress());
+        tvProviderPhoneNumber.setText(providerLoginModel.getPhoneNumber());
+        tvProviderEmail.setText(providerLoginModel.getEmail());
+
+        ivLogout.setOnClickListener(v->{
+            Toast.makeText(requireContext(), "Logout", Toast.LENGTH_SHORT).show();
+        });
+
+        return view;
+    }
+
+    private void initView(View view){
+        tvProviderBusinessDesc = view.findViewById(R.id.tv_provider_profile_business_desc);
+        tvProviderEmail = view.findViewById(R.id.tv_provider_profile_email);
+        tvProviderPhoneNumber = view.findViewById(R.id.tv_provider_profile_phone_number);
+        tvProviderOfficeLocation = view.findViewById(R.id.tv_provider_profile_office_location);
+        tvProviderUsername = view.findViewById(R.id.tv_provider_profile_username);
+        tvProviderFullName = view.findViewById(R.id.tv_provider_profile_full_name);
+        ivLogout = view.findViewById(R.id.iv_provider_profile_logout);
     }
 }
